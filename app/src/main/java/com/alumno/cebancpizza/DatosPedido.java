00b6894a11277f10a59a06902c5f48a804bc0f91
joseDavid83 <joseconesa83@gmail.com>
2015-12-14 22:57:34 +0100

    preparando pestaña pizzas

--- app/src/main/java/com/alumno/cebancpizza/DatosPedido.java
+++ app/src/main/java/com/alumno/cebancpizza/DatosPedido.java
@@ -2,6 +2,10 @@ package com.alumno.cebancpizza;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TabHost;
 
 /**
@@ -9,6 +13,7 @@ import android.widget.TabHost;
  */
 public class DatosPedido extends AppCompatActivity {
     TabHost TbH;
+    private Spinner tip,tam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,45 @@ public class DatosPedido extends AppCompatActivity {
         TbH.addTab(tab1); //añadimos los tabs ya programados
         TbH.addTab(tab2);
 
+        tip=(Spinner)findViewById(R.id.cmbTipo);
+        tam=(Spinner)findViewById(R.id.cmbTamaño);
+
+        ArrayAdapter<CharSequence> adaptadortip = ArrayAdapter.createFromResource(this,R.array.tipo_pizza, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adaptadortam = ArrayAdapter.createFromResource(this,R.array.tamaño_pizza, android.R.layout.simple_spinner_item);
+
+        adaptadortip.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adaptadortam.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        tip.setAdapter(adaptadortip);
+        tam.setAdapter(adaptadortam);
+
+        tip.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //lblMensaje.setText("Seleccionado:" +parent.getItemAtPosition(position));
+                //Podemos recuperar el ítem seleccionado usando
+                //parent.getItemAtPosition(position)
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        tam.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //lblMensaje.setText("Seleccionado:" +parent.getItemAtPosition(position));
+                //Podemos recuperar el ítem seleccionado usando
+                //parent.getItemAtPosition(position)
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
