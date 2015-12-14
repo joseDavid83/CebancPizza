25b716596a6f2100b3c96813005a8da95775ecfb
joseDavid83 <joseconesa83@gmail.com>
2015-12-14 21:50:37 +0100

    TabHost preparado

--- app/src/main/java/com/alumno/cebancpizza/DatosCliente.java
+++ app/src/main/java/com/alumno/cebancpizza/DatosCliente.java
@@ -7,17 +7,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class DatosCliente extends AppCompatActivity{
-    private Button b;
-    private Button c;
-    private Button e;
-    private Button f;
-    private Button h;
+    private Button sig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.datos_cliente);
 
+        sig=(Button)findViewById(R.id.btnSiguiente);
 
+        sig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanzarDatosPedido();
+            }
+        });
+
+    }
+
+    public void lanzarDatosPedido(){
+        Intent i=new Intent(this, DatosPedido.class);
+        startActivity(i);
     }
 
 }
--- app/src/main/java/com/alumno/cebancpizza/DatosPedido.java
+++ app/src/main/java/com/alumno/cebancpizza/DatosPedido.java
@@ -2,15 +2,32 @@ package com.alumno.cebancpizza;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 /**
  * Created by adminportatil on 10/12/2015.
  */
 public class DatosPedido extends AppCompatActivity {
+    TabHost TbH;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedido);
+
+        TbH = (TabHost) findViewById(R.id.tabHost); //llamamos al Tabhost
+        TbH.setup();                                                         //lo activamos
+
+        TabHost.TabSpec tab1 = TbH.newTabSpec("tab1");  //aspectos de cada Tab (pestaña)
+        TabHost.TabSpec tab2 = TbH.newTabSpec("tab2");
+
+        tab1.setIndicator("Pizzas");    //qué queremos que aparezca en las pestañas
+        tab1.setContent(R.id.tab1); //definimos el id de cada Tab (pestaña)
+
+        tab2.setIndicator("Bebidas");
+        tab2.setContent(R.id.tab2);
+
+        TbH.addTab(tab1); //añadimos los tabs ya programados
+        TbH.addTab(tab2);
 
 
     }
